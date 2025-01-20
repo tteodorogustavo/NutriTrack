f71a848f334a5b999d37160f8daee7387bdcca8b
pedro <pedrogoncalvescm@gmail.com>
2025-01-19 21:54:32 -0300

    comit

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -20,7 +20,7 @@ export default function TabTwoScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Testando</ThemedText>
+        <ThemedText type="title">teste</ThemedText>
       </ThemedView>
       <ThemedText>This app includes example code to help you get started.</ThemedText>
       <Collapsible title="File-based routing">
